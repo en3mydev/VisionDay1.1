2b1f0d9b325ff7a847a47d3ffa464e2c128bb295
enemy <nicolae.gheorghe01@stud.etti.upb.ro>
2024-07-02 13:46:38 +0300

    initial

--- obj/Debug/net8.0/VisionDay1.AssemblyInfo.cs
+++ obj/Debug/net8.0/VisionDay1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VisionDay1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdafc34b90e7aa9c86fa664453e3bb3f9ef48b3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("VisionDay1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VisionDay1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
